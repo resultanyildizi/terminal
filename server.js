a1dc6131633b897685d8e6f1a06b8c471a4b1d9d
Resul Tanyıldızı <rsltnyldzz@gmail.com>
2020-04-01 18:16:18 +0300

    interval change 50

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(delay(1000));
 // WebSockets work with the HTTP server
 var io = require("socket.io")(server);
 
-var myInterval = setIntervalwithDelay(heartbeat, 200);
+var myInterval = setIntervalwithDelay(heartbeat, 50);
 
 function heartbeat() {
   io.sockets.emit("heartbeat", players);
