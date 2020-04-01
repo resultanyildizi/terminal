7f42f039e90edab6d4efdc7db1686dfb5dc8123d
Resul Tanyıldızı <rsltnyldzz@gmail.com>
2020-04-01 19:40:06 +0300

    test

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -50,6 +50,7 @@ function setup() {
     }
 
     for (let i = 0; i < players.length; i++) {
+      console.log(playersData[i]);
       if (socket.id != undefined && playersData[i].id != undefined) {
         if (playersData[i].id !== socket.id)
           players[i] = dataToPlayer(playersData[i]);
