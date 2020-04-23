6b98bdb35d8fbb85c41bd044edf8a236ef30a17a
Resul Tanyıldızı <rsltnyldzz@gmail.com>
2020-04-23 17:43:54 +0300

    server speed

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // WebSockets work with the HTTP server
 var io = require("socket.io")(server);
 
-setInterval(heartbeat, 1);
+setInterval(heartbeat, 30);
 
 function heartbeat() {
   io.sockets.emit("heartbeat", players);
